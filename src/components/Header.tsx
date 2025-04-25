9d5cfea940327bcbc597d35844fccb0eaced02b2
thalissongoncalves <thalissongdev@gmail.com>
2025-04-24 22:45:56 -0300

    feat: Cria componente Header com título principal e sub título

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+        <>
+            <h1>Feiras Livres da Cidade</h1>
+            <h2>Conheça as feiras, dias, horários e produtos oferecidos.</h2>
+        </>
+    )
+}
+
+export default Header;
