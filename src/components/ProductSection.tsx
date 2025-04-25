73ca8000a3f8dc53aaf8c59e378ea32c2102b015
thalissongoncalves <thalissongdev@gmail.com>
2025-04-24 23:53:03 -0300

    feat: Adicionado componente ProductSection, onde renderiza um título principal

---
+++ src/components/ProductSection.tsx
@@ -0,0 +1,11 @@
+import Title from "./Title";
+
+function ProductSection() {
+    return(
+        <div data-testid='product-section'>
+            <Title headline='Produtos Locais' />
+        </div>
+    )
+}
+
+export default ProductSection;
