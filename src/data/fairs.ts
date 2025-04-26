09a10d3e80f40103b19ed5e0ff217d29a2fa157c
thalissongoncalves <thalissongdev@gmail.com>
2025-04-26 02:45:56 -0300

    fix: adicionado as imagens de assets/ para public/

--- src/data/fairs.ts
+++ src/data/fairs.ts
@@ -4,27 +4,27 @@ export const fairs = [
         "location": "Praça Benedito Calixto, Pinheiros - São Paulo",
         "day": "Sábado",
         "hours": "9h às 19h",
-        "image": "../src/assets/feira-da-benedito-calixto.jpg"
+        "image": "/feira-da-benedito-calixto.jpg"
     },
     {
         "name": "Feira de Antiguidades do Bixiga",
         "location": "Praça Dom Orione, Bela Vista - São Paulo",
         "day": "Domingo",
         "hours": "8h às 18h",
-        "image": "../src/assets/feira-de-antiguidades-do-bixiga.webp"
+        "image": "/feira-de-antiguidades-do-bixiga.webp"
     },
     {
         "name": "Feira Orgânica do Parque da Água Branca",
         "location": "Av. Francisco Matarazzo, 455 - Barra Funda",
         "day": "Sábado e Domingo",
         "hours": "7h às 13h",
-        "image": "../src/assets/feira-organica-do-parque-da-agua-branca.jpg"
+        "image": "/feira-organica-do-parque-da-agua-branca.jpg"
     },
     {
         "name": "Feira Noturna da Vila Madalena",
         "location": "Rua Fradique Coutinho - Vila Madalena",
         "day": "Quinta-feira",
         "hours": "18h às 22h",
-        "image": "../src/assets/feira-noturna-da-vila-madalena.jpg"
+        "image": "/feira-noturna-da-vila-madalena.jpg"
     }
 ]
--- src/data/products.ts
+++ src/data/products.ts
@@ -3,24 +3,24 @@ export const products = [
         "productName": "Alface Crespa",
         "category": "Verduras",
         "originFair": "Feira da Vila Madalena",
-        "image": "../src/assets/alface.jpg"
+        "image": "/alface.jpg"
     },
     {
         "productName": "Tomate Italiano",
         "category": "Legumes",
         "originFair": "Feira do Parque da Água Branca",
-        "image": "../src/assets/tomate.jpg"
+        "image": "/tomate.jpg"
     },
     {
         "productName": "Queijo Minas Frescal",
         "category": "Laticínios",
         "originFair": "Feira da Praça Benedito Calixto",
-        "image": "../src/assets/queijo.webp"
+        "image": "/queijo.webp"
     },
     {
         "productName": "Mel de Abelha Orgânico",
         "category": "Naturais",
         "originFair": "Feira do Parque da Água Branca",
-        "image": "../src/assets/mel.webp"
+        "image": "/mel.webp"
     }
 ]
